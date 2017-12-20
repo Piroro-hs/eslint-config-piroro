c63af9037b036b18e4bdbaabbd7497d5944b5c61
Piroro-hs <Piroro-hs@users.noreply.github.com>
2017-12-20 17:19:42 +0900

    fix

--- react.js
+++ react.js
@@ -3,4 +3,8 @@ module.exports = {
     'airbnb',
     require.resolve('./index.js'),
   ],
+  rules: {
+    'react/jsx-filename-extension': ['error', {extensions: [".js", ".jsx"]}],
+    'react/prop-types': 'off',
+  }
 };
